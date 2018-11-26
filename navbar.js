be0cd5670a2d92e51b581f788ae9ed8e9e25e8d0
Konrad Semik <konradsemik@gmail.com>
2018-11-26 19:01:57 +0100

    kolkwium1

---
+++ navbar.js
@@ -0,0 +1,9 @@
+let nav='<nav>\n'+
+    '<ul>\n '+
+    '   <li><a href="index.html">home</a></li>\n'+
+    '   <li><a href="blog.html> blog</a>  </li>\n'+
+    '</ul> \n' +
+    '</nav>';
+
+let navBar = document.querySelector('.navbar');
+navBar.innerHTML =nav;
---
+++ script.js
@@ -0,0 +1,5 @@
+<script type="text/javascript" language="javascript">
+    function oznacz(){
+        var pole = document.getElementById("pole");
+        pole.style.background-color = "#cccccc";
+    </script>
